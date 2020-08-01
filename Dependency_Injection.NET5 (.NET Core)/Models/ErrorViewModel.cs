3f43694d444b7538a58e3273a3d9204463e4dcd7
Esraa Mohamed <esraaahmedaliahmed@gmail.com>
2020-08-01 18:36:02 +0200

    Add project files.

---
+++ Dependency_Injection.NET5 (.NET Core)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dependency_Injection.NET5__.NET_Core_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
